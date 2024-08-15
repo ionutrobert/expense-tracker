195ac51df36c091643bc25d2e926adbd6e2c0dee
J92 <ionutrobertbalasoiu@gmail.com>
2024-08-15 16:01:25 +0300

    testing

--- lib/kv.ts
+++ lib/kv.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@vercel/kv';
 
 const kv = createClient({
-  url: process.env.KV_URL, 
+  url: process.env.KV_REST_API_URL, 
   token: process.env.KV_REST_API_TOKEN, 
 });
 
